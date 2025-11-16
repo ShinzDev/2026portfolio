fdf47227f42584b94e9e8343665b7353da3d09b1
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2025-11-16 02:50:10 +0100

    project and file setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './page/Home'
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/page/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Contact from '../components/contact'
+import Projects from '../components/projects'
+
+const Home = () => {
+  return (
+    <div>
+        <Hero/>
+        <Contact/>
+        <Projects/>
+    </div>
+  )
+}
+
+export default Home
