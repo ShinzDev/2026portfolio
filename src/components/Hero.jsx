8c545c3c83856411de743426350b5fdda2fdcf80
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2025-11-19 02:49:47 +0100

    nav done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import pic from '../assets/esc.jpg';
+
+
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='grid grid-col-2 gap-5 '>
+      <div>   hi i am Shina a front-End developer</div>
+      <div>   
+        <img src={pic} width={200} height={300}  alt="" />
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,63 @@
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+
 const Navbar = () => {
-  const link = [
+  const [open, setOpen] = useState(false);
+
+  const links = [
     { name: "Hero", path: "#hero" },
     { name: "Skills", path: "#skills" },
     { name: "Project", path: "#project" },
   ];
-  return (
-    <nav className="grid grid-cols-2">
-      <div>LOGO</div>
 
-      <div>
-        {link.map((links, index) => (
-          <div className=" grid grid-cols-2" key={index}>
-            <a href={links.path}>{links.name}</a>
-          </div>
-        ))}
+  return (
+    <nav className="w-full flex justify-center mt-6">
+      <div className="backdrop-blur-md bg-blue-400  border border-white/20 shadow-lg 
+        rounded-lg px-8 py-3 flex items-center justify-between gap-10 
+        max-w-200 w-full">
+
+        {/* Logo */}
+        <div className="text-xl font-semibold text-white">LOGO</div>
+
+        {/* Desktop Links */}
+        <div className="hidden md:flex items-center gap-8">
+          {links.map((link, index) => (
+            <a
+              key={index}
+              href={link.path}
+              className="text-gray-200 hover:text-blue-400 transition font-medium"
+            >
+              {link.name}
+            </a>
+          ))}
+        </div>
+
+        {/* Mobile Menu Icon */}
+        <button
+          className="md:hidden text-white text-2xl"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <FiX /> : <FiMenu />}
+        </button>
       </div>
+
+      {/* Mobile Dropdown */}
+      {open && (
+        <div className="md:hidden absolute top-24 w-[85%] mx-auto bg-white/10 
+          backdrop-blur-md border border-white/20 shadow-lg rounded-xl p-5 flex flex-col gap-4 text-center">
+
+          {links.map((link, index) => (
+            <a
+              key={index}
+              href={link.path}
+              className="text-gray-200 hover:text-blue-400 transition font-medium text-lg"
+              onClick={() => setOpen(false)}
+            >
+              {link.name}
+            </a>
+          ))}
+        </div>
+      )}
     </nav>
   );
 };
